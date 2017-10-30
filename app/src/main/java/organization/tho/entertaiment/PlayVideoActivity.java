2d56443af836408429d5c5de4e6ca83d0de7055a
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-10-30 15:18:07 +0700

    adding back button on action bar

--- app/src/main/java/organization/tho/entertaiment/PlayVideoActivity.java
+++ app/src/main/java/organization/tho/entertaiment/PlayVideoActivity.java
@@ -1,6 +1,9 @@
 package organization.tho.entertaiment;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
@@ -9,15 +12,17 @@ import com.google.android.gms.ads.MobileAds;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 import com.google.android.youtube.player.YouTubePlayerView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class PlayVideoActivity extends YouTubeBaseActivity
+public class PlayVideoActivity extends AppCompatActivity
         implements YouTubePlayer.OnInitializedListener {
     @BindView(R.id.adView1) AdView mAdView1;
     @BindView(R.id.adView2) AdView mAdView2;
+    @BindView(R.id.toolbar) Toolbar mToolbar;
 
     public static final String API_KEY = "AIzaSyDBRvaEaDuSmQHEcQZrHVOOza_qJQic7NI";
 
@@ -30,8 +35,8 @@ public class PlayVideoActivity extends YouTubeBaseActivity
         ButterKnife.bind(this);
 
         // Initializing YouTube Player View
-        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player);
-        youTubePlayerView.initialize(API_KEY, this);
+        YouTubePlayerSupportFragment fragment = (YouTubePlayerSupportFragment) getSupportFragmentManager().findFragmentById(R.id.youtube_player);
+        fragment.initialize(API_KEY, this);
 
         // get video link from fragment
         getData();
@@ -47,6 +52,21 @@ public class PlayVideoActivity extends YouTubeBaseActivity
         mAdView1.loadAd(adRequest);
         mAdView2.loadAd(adRequest);
 
+        // Action bar
+        setSupportActionBar(mToolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
